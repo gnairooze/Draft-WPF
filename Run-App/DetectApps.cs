10667053ef507f6d09bee1f8563b19988f045d95
George Nairooze <george.nairooze@linkdev.com>
2022-02-17 19:03:52 +0200

    Add project files.

---
+++ Run-App/DetectApps.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run_App
+{
+    internal class DetectApps
+    {
+        public static string DetectAppInstalledPath()
+        {
+            var software = Registry.LocalMachine.OpenSubKey("Software");
+
+            if(software == null)
+            {
+                Console.WriteLine("cannot find software in registry");
+                return string.Empty;
+            }    
+
+            var adobe = software.OpenSubKey("Adobe");
+            if (adobe == null)
+            {
+                Console.WriteLine("cannot find software/Adobe in registry");
+
+                var policies = software.OpenSubKey("Policies");
+
+                if (policies == null)
+                {
+                    Console.WriteLine("cannot find software/Policies in registry");
+                    return string.Empty;
+                }
+                    
+                adobe = policies.OpenSubKey("Adobe");
+                
+                if (adobe == null)
+                {
+                    Console.WriteLine("cannot find software/Policies/Adobe in registry");
+                    return string.Empty;
+                }
+            }
+
+            var adobeAcrobat = adobe.OpenSubKey("Adobe Acrobat");
+
+            if(adobeAcrobat == null)
+            {
+                Console.WriteLine("cannot find ../Adobe/Adobe Acrobat in registry");
+                return string.Empty;
+            }
+
+            var acrobatDC = adobeAcrobat.OpenSubKey("DC");
+
+            if (acrobatDC == null)
+            {
+                Console.WriteLine("cannot find ../Adobe/Adobe Acrobat/DC in registry");
+                return string.Empty;
+            }
+
+            var installPath = acrobatDC.OpenSubKey("InstallPath");
+
+            if (installPath == null)
+            {
+                Console.WriteLine("cannot find ../Adobe/Adobe Acrobat/DC/InstallPath in registry");
+                return string.Empty;
+            }
+
+            var path = installPath.GetValue(string.Empty);
+
+            return path != null ? (string)path : string.Empty;
+        }
+    }
+}
---
+++ Run-App/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Run_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRun_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = this.txtFilePath.Text;
+            string appPath = this.txtAppPath.Text;
+
+            List<string> args = new()
+            {
+                "/A",
+                "navpanes=1",
+                filePath
+            };
+
+            Process p = Process.Start(appPath, args);
+            p.EnableRaisingEvents = true;
+            p.Exited += P_Exited;
+        }
+
+        private void P_Exited(object? sender, EventArgs e)
+        {
+            MessageBox.Show("app exited");
+        }
+
+        private void BtnDetect_Click(object sender, RoutedEventArgs e)
+        {
+            txtAppPath.Text = Path.Combine( DetectApps.DetectAppInstalledPath(), "Acrobat.exe");
+        }
+    }
+}
